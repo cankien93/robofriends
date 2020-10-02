2d533684e770ce6b68a508947a8ab723aa5d11bd
cankien <50330007+cankien93@users.noreply.github.com>
2020-10-02 22:26:26 +0900

    add label to input search box

--- src/component/Card.js
+++ src/component/Card.js
@@ -6,7 +6,7 @@ const Card = ({id, name, email}) => {
     return(
         <div className = 'bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img  alt = 'robo' src={`https://robohash.org/${id}?200x200`} />
-            <h3>{name}</h3>
+            <h2>{name}</h2>
             <p>{email}</p>
         </div>
     );
--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -4,13 +4,15 @@ import React from 'react';
 
 const SearchBox = ({searchfield, searchchange}) => {
     return (
-        <input 
-            type = 'text' 
-            placeholder='input here'
-            onChange = {searchchange}
-        />
+        <div>
+            <label for="search">Search</label>
+            <input 
+                type = 'text' 
+                placeholder='input here'
+                onChange = {searchchange}
+            />
+        </div>        
     )
-
 };
 
 export default SearchBox;
