bf4bd33c266ffe79a78d484a73f2bc4812d63849
cankien <50330007+cankien93@users.noreply.github.com>
2020-10-07 07:52:05 +0900

    change size roo picture

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,7 +5,7 @@ const Card = ({id, name, email}) => {
     
     return(
         <div className = 'bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img  alt = 'robo' src={`https://robohash.org/${id}?200x200`} />
+            <img  alt = 'robo' src={`https://robohash.org/${id}?size=200x200`} />
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
